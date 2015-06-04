e216bc448098df8220f170b9d69e288c1257a7b5
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-04 10:15:06 -0700

    created tweet migration file

---
+++ migrations/20150604101019_tweets.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('tweets', function(table) {
+    table.increments('id').primary();
+    table.string('username');
+    table.string('tweet');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('tweets');
+};
