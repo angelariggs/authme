c2d1c483e865552e68a2aebb9bc657d2e4cd588d
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-04 14:20:36 -0700

    added unique to users and added table reference to tweets

--- migrations/20150604101019_tweets.js
+++ migrations/20150604101019_tweets.js
@@ -2,8 +2,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tweets', function(table) {
     table.increments('id').primary();
-    table.string('username');
+    table.integer('userID').references('id').inTable('users');
     table.string('tweet');
+    table.dateTime('timestamp');
   });
 };
 
